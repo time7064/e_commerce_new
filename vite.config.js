3ccb905b2a04e3576ce66400491e9341b5bc16f8
time7064 <time7064@gmail.com>
2026-05-09 08:15:25 +0900

    change base url

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base:"/e_commerce_new",
-  base:"/",
+  base:"/e_commerce_new",
 })
