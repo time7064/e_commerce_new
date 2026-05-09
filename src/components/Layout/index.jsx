7eacb0d083596db9939151aaaf361359dc328c17
time7064 <time7064@gmail.com>
2026-05-09 17:32:18 +0900

    Change text

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -9,7 +9,7 @@ const Layout = () => {
             {/* <Link to="/"  */}
             <Link to="/e_commerce_new/" 
                 className={ styles.link}>
-                <h1>CSY_mall</h1>
+                <h1>갓성비 mall</h1>
             </Link>
 
             {/*             
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/e_commerce_new",  // 서버 배포 관련 추가
+  base:"/e_commerce_new",  // 서버 배포 관련 추가 - 원인
 })
