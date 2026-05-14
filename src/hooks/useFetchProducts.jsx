359e4e31ab5bfb436dc09292533da78cea706b4b
time7064 <time7064@gmail.com>
2026-05-14 16:37:39 +0900

    Change text

--- src/hooks/useFetchProducts.jsx
+++ src/hooks/useFetchProducts.jsx
@@ -12,7 +12,32 @@ function useFetchProducts() {
             try {
                 setIsProductsLoading(true);
 
-                const response = await fetch("http://localhost:3000/products");
+                // TODO : 서버 배포 휴대폰 접속 문제 해결 ...............................................
+                // 현재 도메인 가져오기 (예: example.com)
+                const domain = window.location.hostname;
+                // 포트 번호를 포함한 호스트 가져오기 (예: localhost:3000)
+                const host = window.location.host;
+                // 프로토콜(http/https)을 포함한 전체 URL 가져오기
+                const href = window.location.href;
+                console.log("domain : ", domain);
+                console.log("host : ", host);
+                console.log("href : ", href);
+
+                const URL = href + "products";
+                console.log("URL : ", URL);
+
+                var vURL = "http://localhost:3000/products";
+
+                if(domain != "localhost") {
+                    vURL = "https://time7064.github.io/e_commerce_new/products"
+                }
+
+                console.log("vURL : ", vURL);
+                // const response = await fetch("http://localhost:3000/products");
+                const response = await fetch(vURL);
+                
+                // ......................................................................................
+
                 const json = await response.json();
                 console.log(json);
 
