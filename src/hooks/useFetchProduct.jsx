cf93077c2e06a5fea09da3837e85bc91da8d3ff2
time7064 <time7064@gmail.com>
2026-06-09 19:18:50 +0900

    change name

--- src/hooks/useFetchProduct.jsx
+++ src/hooks/useFetchProduct.jsx
@@ -32,7 +32,7 @@ function useFetchProduct(productId) {
 
                     // TODO: URL 주소로 data 못찾음.. 필터링으로..
                     // var json2 = json.filter(value  => value.id === productId);
-                    var json2 = json.filter(value  => value.id == productId);
+                    var json2 = json.filter(value  => value.id == String(productId));
 
 
                     console.log("[reviews json2] \n", json2);
